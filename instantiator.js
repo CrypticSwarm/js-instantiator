c95a21a6b5d6495012efdac60155294e9c205a11
CrypticSwarm <crypticswarm+git@gmail.com>
2012-04-29 17:11:51 -0500

    don't slice off front of key

--- instantiator.js
+++ instantiator.js
@@ -8,9 +8,9 @@ module.exports = function instantiate(template, vars) {
   return traverse(template).map(function (value) {
     if (this.notRoot && isVar(this.key)) {
       this.remove()
-      this.key = vars[this.key.slice(1)]
+      this.key = vars[this.key]
       this.update(value)
     }
-    if (this.isLeaf && isVar(value)) this.update(vars[value.slice(1)])
+    if (this.isLeaf && isVar(value)) this.update(vars[value])
   })
 }
